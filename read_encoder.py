e6aea7091a1317168d93125c9469756212566b5e
jam326 <61119861+jam326@users.noreply.github.com>
2020-02-16 23:40:21 -0500

    Update read_encoder.py

--- read_encoder.py
+++ read_encoder.py
@@ -17,39 +17,43 @@ encoderStatus = 0
 Pin_motor_encoderA = 13
 Pin_motor_encoderB = 6
 
-#create callback function
-def updateMotorPosition(motorPosition,encoderStatus):
+#create function for edge detect
+def updateMotorPosition(position,status):
     #bitwise shift to left by  1 bit
-    encoderStatus <<= 1
+    status <<= 1
     #read channel A and put that value into the rightmost bit of encoderStatus
-    encoderStatus |= GPIO.input(Pin_motor_encoderA)
+    status |= GPIO.input(Pin_motor_encoderA)
     #bitwise shift to left by 1 bit
-    encoderStatus <<= 1
-    #read channel B and put that value into the the rightmost bit of encoderStatus
-    encoderStatus |= GPIO.input(Pin_motor_encoderB)
+    status <<= 1
+    #read channel B and put that value into the 
+    status |= GPIO.input(Pin_motor_encoderB)
     #truncate encoder status to rightmost 4 bits
-    encoderStatus &= 15
+    status &= 15
     #if encoder status matches pattern for counting up by one, add one to motorPosition
-    if encoderStatus == 2 or encoderStatus == 4 or encoderStatus == 11 or encoderStatus == 13:
-         motorPosition += 1
+    if status == 2 or status == 4 or status == 11 or status == 13:
+         position += 1
     else:
         #otherwise subtract one (since function is only called if something changes)
-        motorPosition -= 1
+        position -= 1
+    return [position,status]
 
-#Set up pins
-GPIO.setup(Pin_motor_encoderA,GPIO.IN,pull_up_down = GPIO.PUD_UP) 
-GPIO.setup(Pin_motor_encoderB,GPIO.IN,pull_up_down = GPIO.PUD_UP) 
+
+#Set up -run once
+GPIO.setup(Pin_motor_encoderA,GPIO.IN,pull_up_down = GPIO.PUD_UP) #change to GPIO.IN
+GPIO.setup(Pin_motor_encoderB,GPIO.IN,pull_up_down = GPIO.PUD_UP) #change to GPIO.IN
 
 
 #interrupt for encoder pins
-GPIO.add_event_detect(Pin_motor_encoderA,GPIO.BOTH,callback = updateMotorPosition) 
-GPIO.add_event_detect(Pin_motor_encoderB,GPIO.BOTH,callback = updateMotorPosition)
+GPIO.add_event_detect(Pin_motor_encoderA,GPIO.BOTH,callback = updateMotorPosition) #add GPIO.BOTH, add callback = 
+GPIO.add_event_detect(Pin_motor_encoderB,GPIO.BOTH,callback = updateMotorPosition) #add GPIO.BOTH, add callback = 
+
 
 try:
     while True:
-        updateMotorPosition(motorPosition,encoderStatus)
+        [motorPosition,encoderStatus] = updateMotorPosition(motorPosition,encoderStatus)
         print(motorPosition)
+        print(encoderStatus)
 except KeyboardInterrupt:
-        GPIO.cleanup()
+        print('done')
         
 
