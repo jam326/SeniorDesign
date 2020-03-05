70ed893766975d1dbf9780ca9e5b1ba411f48c26
jam326 <61119861+jam326@users.noreply.github.com>
2020-03-05 14:44:56 -0500

    Add files via upload

--- test_encoder.py
+++ test_encoder.py
@@ -3,17 +3,17 @@ import time
 GPIO.setmode(GPIO.BOARD)
 from helper import Motor
 
-pinA =
-pinB =
-pinPWM =
-pinDIR1 =
-pinDIR2 =
+pinA = 38
+pinB = 40
+pinPWM = 8
+pinDIR1 = 10
+pinDIR2 = 12
 
 motor = Motor(pinA,pinB,pinPWM,pinDIR1,pinDIR2)
 GPIO.add_event_detect(pinA, GPIO.BOTH, callback = motor.updateposition)  # add GPIO.BOTH, add callback =
 GPIO.add_event_detect(pinB, GPIO.BOTH, callback = motor.updateposition)  # add GPIO.BOTH, add callback =
 
-try: 
+try:
     while True:
         print(motor.position)
 except: KeyboardInterrupt
