975d2dd9c889c67dbea408970fc8c1df974d21a9
jam326 <61119861+jam326@users.noreply.github.com>
2020-03-05 15:40:13 -0500

    Add files via upload

--- test_encoder.py
+++ test_encoder.py
@@ -10,6 +10,8 @@ pinDIR1 = 10
 pinDIR2 = 12
 
 motor = Motor(pinA,pinB,pinPWM,pinDIR1,pinDIR2)
+GPIO.setup(pinA, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(pinB, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.add_event_detect(pinA, GPIO.BOTH, callback = motor.updateposition)  # add GPIO.BOTH, add callback =
 GPIO.add_event_detect(pinB, GPIO.BOTH, callback = motor.updateposition)  # add GPIO.BOTH, add callback =
 
